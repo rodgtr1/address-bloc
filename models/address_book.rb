64a37156732405d573bb4833f61dbe2dd0e2016f
Travis Rodgers <rodgtr1@gmail.com>
2017-02-21 17:15:19 -0500

    created a remove_entry

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,4 +17,15 @@ class AddressBook
     end
     entries.insert(index, Entry.new(name, phone_number, email))
   end
+
+  def remove_entry(name, phone_number, email)
+    delete_entry = nil
+
+    @entries.each do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        delete_entry = entry
+      end
+    end
+    @entries.delete(delete_entry)
+  end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,4 +36,21 @@ RSpec.describe AddressBook do
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
     end
   end
+
+  describe "#remove_entry" do
+    it "removes an entry using the name, phone_number and email address" do
+      book = AddressBook.new
+      book.add_entry('Travis Rodgers', '010.012.1822', 'travis.rodgers@tech.com')
+
+      name = 'Ada Lovelace'
+      phone_number = '010.012.1815'
+      email = 'augusta.king@lovelace.com'
+      book.add_entry(name, phone_number, email)
+
+      expect(book.entries.size).to eq(2)
+      book.remove_entry(name, phone_number, email)
+      expect(book.entries.size).to eq(1)
+      expect(book.entries.first.name).to eq('Travis Rodgers')
+    end
+  end
 end
