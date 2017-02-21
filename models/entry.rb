14a395d41bd7e8b891a02399b1d00d4d4f040330
Travis Rodgers <rodgtr1@gmail.com>
2017-02-21 16:42:45 -0500

    created models and model tests

---
+++ models/entry.rb
@@ -0,0 +1,14 @@
+class Entry
+  #must be accessors since we mutate them
+  attr_accessor :name, :phone_number, :email
+
+  def initialize(name, phone_number, email)
+    @name = name
+    @phone_number = phone_number
+    @email = email
+  end
+
+  def to_s
+    "Name: #{name}\nPhone Number: #{phone_number}\nEmail: #{email}"
+  end
+end
