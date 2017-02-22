5e2c6b0a848ced0f6e7b168b1e1452783eaaa6b8
Travis Rodgers <rodgtr1@gmail.com>
2017-02-21 22:04:44 -0500

    created menu and submenu

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,11 @@
+require_relative 'controllers/menu_controller'
+
+#creates new MenuController when AddressBloc starts
+menu = MenuController.new
+
+#clears the command line
+system "clear"
 puts "Welcome to AddressBloc!"
+
+#calls main_menu to display menu
+menu.main_menu
