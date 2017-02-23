d4822d653974aadbd8eeaf66ef6d7203c3654d13
Travis Rodgers <rodgtr1@gmail.com>
2017-02-22 22:28:24 -0500

    testing with entries_2.csv

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -48,7 +48,6 @@ RSpec.describe AddressBook do
 
       expect(book_size).to eq 5
     end
-end
 
     it "imports the 1st entry" do
       book.import_from_csv("entries.csv")
@@ -84,6 +83,37 @@ end
       entry_five = book.entries[4]
       check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
     end
+  end
+
+  describe "importing from entries_2.csv" do
+    it "imports the correct number of entries" do
+      book.import_from_csv("entries_2.csv")
+      book_size = book.entries.size
+
+      expect(book_size).to eq 3
+    end
+
+    it "imports the 1st entry" do
+      book.import_from_csv("entries_2.csv")
+      #Check the first entry
+      entry_one = book.entries[0]
+      check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+    end
+
+    it "imports the 2nd entry" do
+      book.import_from_csv("entries_2.csv")
+      # Check the second entry
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+    end
+
+    it "imports the 3rd entry" do
+      book.import_from_csv("entries_2.csv")
+      # Check the third entry
+      entry_three = book.entries[2]
+      check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+    end
+  end
 
 
   describe "#remove_entry" do
