9a091ec86a45caeaee4cbec1430f494cbbc40dc7
Travis Rodgers <rodgtr1@gmail.com>
2017-02-24 15:52:53 -0500

    configured binary search

--- models/address_book.rb
+++ models/address_book.rb
@@ -39,4 +39,29 @@ class AddressBook
     end
     @entries.delete(delete_entry)
   end
+
+  #Search AddressBook for a specific entry by name
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+
+    # #2
+    while lower <= upper
+      # #3
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+      # #4
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+    # #5
+    return nil
+  end
 end
