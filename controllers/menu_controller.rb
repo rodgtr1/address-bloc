36a3454c456913c306d94c7eb9c6adfb93859d5d
Travis Rodgers <rodgtr1@gmail.com>
2017-02-24 21:53:59 -0500

    finished menu

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -53,6 +53,7 @@ class MenuController
      end
    end
 
+
   def view_all_entries
     address_book.entries.each do |entry|
       system "clear"
@@ -82,9 +83,60 @@ class MenuController
   end
 
   def search_entries
+    print "Search by name: "
+    name = gets.chomp
+
+    match = address_book.binary_search(name)
+    system "clear"
+
+    if match
+      puts match.to_s
+      search_submenu(match)
+    else
+      puts "No match found for #{name}"
+    end
   end
 
   def read_csv
+    print "Enter CSV file to import: "
+     file_name = gets.chomp
+
+     if file_name.empty?
+       system "clear"
+       puts "No CSV file read"
+       main_menu
+     end
+
+     begin
+       entry_count = address_book.import_from_csv(file_name).count
+       system "clear"
+       puts "#{entry_count} new entries added from #{file_name}"
+     rescue
+       puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+       read_csv
+     end
+  end
+
+  def delete_entry(entry)
+    address_book.entries.delete(entry)
+    puts "#{entry.name} has been deleted"
+  end
+
+  def edit_entry(entry)
+    print "Updated name: "
+    name = gets.chomp
+    print "Updated phone number: "
+    phone_number = gets.chomp
+    print "Updated email: "
+    email = gets.chomp
+
+    entry.name = name if !name.empty?
+    entry.phone_number = phone_number if !phone_number.empty?
+    entry.email = email if !email.empty?
+    system "clear"
+
+    puts "Updated entry:"
+    puts entry
   end
 
   def entry_n_submenu
@@ -114,8 +166,11 @@ class MenuController
     when "n"
 
     when "d"
+      delete_entry(entry)
 
     when "e"
+      edit_entry(entry)
+      entry_submenu(entry)
 
     when "m"
       system "clear"
@@ -124,6 +179,33 @@ class MenuController
       system "clear"
       puts "#{selection} is not a valid input"
       entry_submenu(entry)
+    end
+  end
+
+  def search_submenu(entry)
+    puts "\nd = delete entry"
+    puts "e - edit this entry"
+    puts "m - return to main menu"
+
+    selection = gets.chomp
+
+  case selection
+    when "d"
+      system "clear"
+      delete_entry(entry)
+      main_menu
+    when "e"
+      edit_entry(entry)
+      system "clear"
+      main_menu
+    when "m"
+      system "clear"
+      main_menu
+    else
+      system "clear"
+      puts "#{selection} is not a valid input"
+      puts entry.to_s
+      search_submenu(entry)
     end
   end
  end
